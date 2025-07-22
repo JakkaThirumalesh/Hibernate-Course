98b08dd5a87ac155e8f1f7fb6901cc3dd89e298c
JakkaThirumalesh <jakkathirumalesh342@gmail.com>
2025-07-23 00:12:44 +0530

    level-1 commit

---
+++ src/main/java/com/hiber/App.java
@@ -0,0 +1,21 @@
+package com.hiber;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+//        Configuration cfg = new Configuration();
+//        cfg.configure();
+//        
+//        SessionFactory factory = cfg.buildSessionFactory();
+//        System.out.println(factory);
+        
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        System.out.println(factory);
+    }
+}
