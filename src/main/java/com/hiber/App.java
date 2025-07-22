e07266fe7dd9552ed849d2cf092c15696d5c3bf5
JakkaThirumalesh <jakkathirumalesh342@gmail.com>
2025-07-23 03:45:20 +0530

    file base configuration

--- src/main/java/com/hiber/App.java
+++ src/main/java/com/hiber/App.java
@@ -8,14 +8,22 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
+        //--------------------------------
 //        Configuration cfg = new Configuration();
 //        cfg.configure();
 //        
 //        SessionFactory factory = cfg.buildSessionFactory();
 //        System.out.println(factory);
         
-        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        //--------------------------------
+//        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+//        System.out.println(factory);
+        
+        //--------------------------------
+        
+        SessionFactory factory = HibernateUtil.getSessionFactory();
         System.out.println(factory);
+        
+        
     }
 }
